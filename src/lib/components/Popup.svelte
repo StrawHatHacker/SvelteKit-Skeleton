55e094a7cc0476b2ff601e27705b8dc895c05681
strawhathacker <ntahsp@gmail.com>
2022-03-23 20:15:33 +0200

    Added Popup component

---
+++ src/lib/components/Popup.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { center } from '../utils/animations';
+	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	const dispatch = createEventDispatcher();
+	const handleChange = () => dispatch('change', false);
+
+	export let show: boolean;
+	export const position = 'center';
+	export let duration = 300;
+	export let delay = 0;
+</script>
+
+{#if show}
+	<div
+		class="shade"
+		transition:fade={{ duration, delay }}
+		on:click={handleChange}
+		on:touchmove|preventDefault
+	/>
+	<div transition:center={{ duration, delay }} class="content center">
+		<slot />
+	</div>
+{/if}
+
+<style lang="scss">
+	@use '../../styles/colors.scss' as *;
+	@use '../../styles/mixins.scss' as *;
+
+	.content {
+		width: max-content;
+		max-width: 90%;
+		overflow: hidden;
+		position: fixed;
+		z-index: 11;
+		// background-color: $main-black;
+		opacity: 1;
+		padding: 1rem;
+		border-radius: 0.5rem;
+		// border: 3px solid $background-black;
+		// box-shadow: 3px 3px 5px $main-black;
+	}
+
+	.shade {
+		position: fixed;
+		z-index: 10;
+		inset: 0;
+		// background-color: $main-black;
+		opacity: 0.7;
+	}
+
+	.center {
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		transform-origin: 0 0;
+	}
+</style>
