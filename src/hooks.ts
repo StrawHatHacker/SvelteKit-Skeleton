b8462e34045c2d44b0444facd06a6776c1fae037
strawhathacker <ntahsp@gmail.com>
2022-03-24 16:48:41 +0200

    environment variable support

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,9 @@
 import { fetchSession } from '$lib/utils/sessionHandler';
 import type { Handle, GetSession } from '@sveltejs/kit';
 import cookie from 'cookie';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const handle: Handle | unknown = async ({ event, resolve }) => {
     const cookies = cookie.parse(event.request.headers.get('cookie') || '');
