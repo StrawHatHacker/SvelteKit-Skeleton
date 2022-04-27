652ad934c2dd5672094056582605093e89c42f24
strawhathacker <ntahsp@gmail.com>
2022-04-27 11:54:07 +0300

    Element examples at /index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,9 +28,39 @@
 	<button on:click={login}>Login</button>
 	<button on:click={logout}>Logout</button>
 	<button on:click={() => (window.location.href = '/protected')}>Goto protected</button>
+	<input type="text" placeholder="Placeholder"/>
+	<input type="radio" />
+	<h1>H1</h1>
+	<h2>H2</h2>
+	<h3>H3</h3>
+	<h4>H4</h4>
+	<h5>H5</h5>
+	<h6>H6</h6>
+	<p>p</p>
+	<a href="/test">Link to /test</a>
+	<ul>
+		<li>First unordered list element</li>
+		<li>Second unordered list element</li>
+	</ul>
+	<ol>
+		<li>First ordered list element</li>
+		<li>Second ordered list element</li>
+	</ol>
+	<dl>
+		<dt>Definition title</dt>
+		<dd>First Definition list element</dd>
+		<dd>Second Definition list element</dd>
+	</dl>
+	<progress id="upload" max="100" value="70"> 70% </progress>
+	<textarea name="textarea" id="textarea" cols="30" rows="10">Hi I'm a textarea :)</textarea>
+	<dialog>ok</dialog>	
 </main>
 
 <footer />
 
-<style>
+<style lang="scss">
+	main > * {
+		display: block;
+		margin-bottom: 1rem;
+	}
 </style>
