30814fe0c34b4731535913f6c18ced18c1aeb355
strawhathacker <ntahsp@gmail.com>
2022-04-27 11:53:48 +0300

    Layout with premade head

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Skeleton 🦴💀🦴</title>
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+	<meta name="copyright" content="" />
+	<meta name="generator" content="svelte" />
+	<meta http-equiv="content-language" content="en-us" />
+	<meta name="og:title" content="" />
+	<meta name="twitter:title" content="" />
+	<meta name="og:description" content="" />
+	<meta name="twitter:description" content="" />
+	<!-- <meta property="og:image" content="%svelte.assets%/logo.png" /> -->
+</svelte:head>
+
+<slot />
