78c15e8b37e164fe68b727330a52662e6c4215ee
strawhathacker <ntahsp@gmail.com>
2022-09-03 11:57:44 +0300

    Using `invalidateAll` to rerun server logic

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { goto, invalidateAll } from '$app/navigation';
+
 	import { post } from '$lib/utils/requestMethods';
 	import type { Iresponse } from 'src/interfaces';
 
 	// export let data;
-
+	
 	const logout = async () => {
 		const res: Iresponse<{}> = await post('/api/v1/user/logout');
-
-		window.location.replace('/');
+		await invalidateAll();
+		goto('/')
 	};
 </script>
 
