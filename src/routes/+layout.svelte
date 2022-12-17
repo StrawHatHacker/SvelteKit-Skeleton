1d6c041eba5735305d569a6701db44455cdb957e
Skillers3 <ntahsp@gmail.com>
2022-12-17 11:28:19 +0000

    Remove warnings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,6 @@
 	<meta name="author" content="" />
 	<meta name="copyright" content="" />
 	<meta name="generator" content="svelte" />
-	<meta http-equiv="content-language" content="en-us" />
 	<meta name="og:title" content="" />
 	<meta name="twitter:title" content="" />
 	<meta name="og:description" content="" />
--- src/routes/protected/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div data-sveltekit-reload>
-	<slot />
-</div>
