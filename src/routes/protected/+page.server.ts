57835470f36e50dfba0607ff5e1591411eef27dd
strawhathacker <ntahsp@gmail.com>
2023-07-24 20:29:30 +0300

    Helpful comments :)

--- src/routes/protected/+page.server.ts
+++ src/routes/protected/+page.server.ts
@@ -2,6 +2,11 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
+
+	// We don't need to check if the session is valid here.
+	// If it is valid hooks.server.ts will give us a "session.id" below.
+	// If it is not present here that means the user isn't authenticated,
+	// and we can handle what happens with that information here.
 	if (!locals.session?.id) throw redirect(302, '/login');
 
 	// Passing data to +page.ts
