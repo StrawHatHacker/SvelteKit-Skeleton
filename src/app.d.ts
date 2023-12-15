3ea4dd75c81dc0e660ab05db98c3c7cc0bed9b4a
Skillers3-Panos <ntahsp@gmail.com>
2023-12-15 09:14:13 +0200

    App.d.ts allows for external interfaces

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,14 +2,18 @@
 
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
-declare namespace App {
-	interface Locals {
-		session: {
-			id: string;
-		} | null;
-	}
+declare global {
+	namespace App {
+		interface Locals {
+			session: {
+				id: string;
+			} | null;
+		}
 
-	interface PrivateEnv {
-		MONGO_URI: string;
+		interface PrivateEnv {
+			MONGO_URI: string;
+		}
 	}
 }
+
+export { }
